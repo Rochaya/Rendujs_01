b64d6b051ef78e1ea10fd04d381e7d5ae68e5192
Rochaya <rochaya@DESKTOP-OCT9BF6.localdomain>
2023-07-12 04:22:28 +0200

    exo 5 fini

---
+++ exo_05.js
@@ -0,0 +1,23 @@
+const numbers1 = [10, 15, 3, 7];
+const numbers2 = [1, 8, 10, 21];
+const k = 17;
+const j = 19;
+
+function check(tab, nb) {
+    const length = tab.length;
+    const numbers = new Set(); // Initialisation d'un ensemble vide pour stocker les nombre visiter au fur et a mesure.
+  
+    for (let i = 0; i < length; i++) {
+        const complement = nb - tab[i];
+
+        if (numbers.has(complement)) { // Verifie si le complement obtenu est present dans l'ensemble numbers.
+            return true;
+        }
+        numbers.add(tab[i]); // ajoute chaque nombre visiter dans le tab a l'ensemble numbers.
+    }
+    
+    return false;
+  }
+  
+  console.log(check(numbers1, k));
+  console.log(check(numbers2, j));
